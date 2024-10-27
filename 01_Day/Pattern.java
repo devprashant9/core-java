607af919e87b1f7066754ced83fa4165602cc7fb
devprashant <kumarjha183@gmail.com>
2024-10-27 09:08:00 +0530

    Challenges Completed

---
+++ 01_Day/Pattern.java
@@ -0,0 +1,28 @@
+public class Pattern {
+    public static void main(String[] args) {
+        // Pattern 1
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+
+        // Pattern 2
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println();
+
+        // Pattern 3
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
